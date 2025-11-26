3fe6acec00c55ca3f7566bff873d17d2ae8b2920
johnlago <johlag@gmail.com>
2025-11-25 22:18:50 -0800

    fix: Add additional files to uninstaller scripts

--- uninstall_ezshare.sh
+++ uninstall_ezshare.sh
@@ -3,4 +3,7 @@
 rm -rf $HOME/.venv/ezshare_resmed
 rm $HOME/.local/bin/ezshare_resmed
 rm $HOME/.local/bin/ezshare_resmed.py
+rm $HOME/.local/bin/upload_tracker.py
+rm $HOME/.local/bin/sleephq_uploader.py
+rm $HOME/.local/bin/sleephq_client.py
 echo "ezshare_resmed uninstalled"
